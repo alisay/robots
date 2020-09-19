b0036d52f2faa55a5658dcb0bd35ecefa5fd06f9
Alisa <alisa.blakeney@gmail.com>
2020-09-19 17:41:25 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const { start } = require('repl');
+const Robot = require("./robot");
+fs = require('fs');
+
+const roboguy = new Robot();
+const inputFile = process.argv[2];
+
+const takeCommand = (file)=>{
+    fs.readFile(file, 'utf8', (err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        parsedData = data
+            .toLowerCase()
+            .split(/\n/)
+            .map(datum=>datum.trim());
+        console.log(parsedData);
+
+        const startingIndex = parsedData.findIndex(element=> element.startsWith('place'));
+
+        for (let index = startingIndex; index < parsedData.length; ++index){
+            let element = (parsedData[index]);
+            switch(parsedData[index]){
+                case "move":
+                    roboguy.move();
+                    break;
+                case "left":
+                    roboguy.left();
+                    break;
+                case "right":
+                    roboguy.right();
+                    break;
+                default:
+                    if (element.startsWith("place")){
+                        arguments = element.slice(6).split(",")
+                        roboguy.place(arguments)
+                    }
+                    "out of moves"
+            }
+        }
+    });    
+}
+
+
+console.log(takeCommand(inputFile));
+
---
+++ robot.js
@@ -0,0 +1,44 @@
+class Robot {
+    place(xyf){
+        this.x = xyf[0]
+        this.y = xyf[1]
+        this.f = xyf[2]    
+    }
+
+    move(){
+        switch (this.f){
+            case "north":
+                this.y++;
+                break;
+            case "south":
+                this.y--;
+                break;
+            case "east":
+                this.x++;
+                break;
+            case "west":
+                this.x--;
+                break;
+            default:
+                "invalid input";
+        }
+    }
+
+    left(){
+        const f=["north","east","south","west"]
+        const index = ((f.indexOf(this.f)-1)+4)% 4
+        this.f = f[index];
+    }
+
+    right(){
+        const f=["north","east","south","west"]
+        const index = ((f.indexOf(this.f)+1)% 4)
+        this.f = f[index];
+    }
+
+    report(){
+        console.log(`${this.x}, ${this.y}, ${this.f.toUpperCase()}`);//announce the X,Y and F of the robot
+    }
+}
+
+module.exports = Robot;
